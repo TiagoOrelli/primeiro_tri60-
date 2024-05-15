f4265cf081e26272e2f73a583ff44978affe90eb
TiagoOrelli <160608111+TiagoOrelli@users.noreply.github.com>
2024-05-14 22:16:43 -0300

    update

---
+++ main.js
@@ -0,0 +1,11 @@
+
+conts botoes = document.querySelectorAll("botao");
+ for(let i=0;i <botoes.lenth;i++){
+    botoes {i} .onclick =function(){ 
+
+        for(let j=0; j<botoes.lenth;j++)
+            botoes[j].classlist.remove("ativo"):
+
+        botoes{i}.classlist.add("ativo");
+    }
+ }
